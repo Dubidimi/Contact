ce3a872ef99012686cccb5a632912c0b412fcec8
Dimitri V <dvardidze15@gmail.com>
2024-07-22 18:39:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function choosegeneral() {
+    document.getElementById('general').style.backgroundColor = "#e0f1e7"
+    document.getElementById('general').style.borderColor= "#0c7d69"
+}
+function choosesupport() {
+    document.getElementById('support').style.backgroundColor = "#e0f1e7"
+    document.getElementById('support').style.borderColor= "#0c7d69"
+}
+function openPopup() {
+    document.getElementById('success__message').style.display = "block";
+}
